b97af9ff0edeb48102b7c4daea993edf72543e4c
CODETHVRD <talzsaad@gmail.com>
2024-04-06 19:31:38 +0100

    commit message

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -23,6 +23,7 @@ def main():
 	expenses = []
 	while True:
 		print('\nExpense Tracker')
+		print('1. Add an expense')
 		print('2. List all expenses')
         	print('3. Show total expenses')
         	print('4. Filter expenses by category')
@@ -38,7 +39,7 @@ def main():
             			print_expenses(expenses)
     
         		elif choice == '3':
-				print('\nTotal Expenses:', total_expenses(expenses))
+				print('\nTotal Expenses:',total_expenses(expenses))
     
        		 	elif choice == '4':
             			category = input('Enter category to filter: ')
