30851135da9f3c58b5c8ee07a4a4dfaf3ed6728c
CODETHVRD <160733800+CODETHVRD@users.noreply.github.com>
2024-04-11 13:05:46 +0000

    updated indentation error

--- Case_Converter.py
+++ Case_Converter.py
@@ -11,8 +11,7 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
 
 #main function that calls the other functions
 def main():
-	convert_to_snake_case()
-	print(convert_to_snake_case('aLongAndComplexString'))
+	print(convert_to_snake_case('mYNameIsTalHaHSaaD'))
 
 if __name__=='__main__':
 	main()
